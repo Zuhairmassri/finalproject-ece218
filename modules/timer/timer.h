edef1f1858768578317737c6d741d2b3cdb91e5b
saldrich13 <aldrichs@union.edu>
2024-03-07 03:10:26 +0000

    Timer - SA

---
+++ modules/timer/timer.h
@@ -0,0 +1,30 @@
+//=====[#include guards - begin]===============================================
+
+#ifndef TIMER_H
+#define TIMER_H
+
+//=====[Declaration of public defines]=========================================
+
+//=====[Declaration of public data types]======================================
+class Timer {
+public:
+    Timer(); // Constructor
+    void timerInit(); // Initializes the timer without setting a duration
+    void start(long duration); // Starts the timer with a specified duration in milliseconds
+    bool hasExpired(); // Checks if the timer has exceeded its duration
+    void reset(); // Resets the timer to its initial state
+
+
+private:
+    Timer(const Timer&); // Prevent copying
+    Timer& operator=(const Timer&); // Prevent assignment
+
+    long startTime; // Start time of the timer
+    long duration; // Duration of the timer
+    bool running; // Indicates if the timer is running
+};
+//=====[Declarations (prototypes) of public functions]=========================
+
+//=====[#include guards - end]=================================================
+
+#endif // TIMER_H
