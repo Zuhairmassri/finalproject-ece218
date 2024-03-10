2055b2f9914bb99d681ac1e63e8ac3a39cdf358f
Zuhairmassri <136488741+Zuhairmassri@users.noreply.github.com>
2024-03-09 22:26:23 -0500

    Update timer.h for the millionth time

--- modules/timer/timer.h
+++ modules/timer/timer.h
@@ -1,31 +1,25 @@
-//=====[#include guards - begin]===============================================
+#ifndef TIMER_H
+#define TIMER_H
 
-#ifndef TIMERS_H
-#define TIMERS_H
+#include "mbed.h"
+#include "RealTimeClock.h"
+#include <chrono>
 
-//=====[Declaration of public defines]=========================================
-
-//=====[Declaration of public data types]======================================
 class Timers {
 public:
-    Timers(); // Constructor
-    void timerInit(); // Initializes the timer without setting a duration
-    void start(long duration); // Starts the timer with a specified duration in milliseconds
-    bool hasExpired(); // Checks if the timer has exceeded its duration
-    void reset(); // Resets the timer to its initial state
-    bool isRunning();
+    Timers();
+    ~Timers();
 
+    void timerInit();
+    void start(long seconds);
+    int hasExpired();
+    bool isRunning();
+    void reset();
 
 private:
-    Timers(const Timers&); // Prevent copying
-    Timers& operator=(const Timers&); // Prevent assignment
-
-    long startTime; // Start time of the timer
-    long duration; // Duration of the timer
-    bool running; // Indicates if the timer is running
+    mbed::RealTimeClock::time_point startTime;
+    std::chrono::seconds duration;
+    bool running;
 };
-//=====[Declarations (prototypes) of public functions]=========================
 
-//=====[#include guards - end]=================================================
-
-#endif // TIMERS_H
+#endif // TIMER_H
